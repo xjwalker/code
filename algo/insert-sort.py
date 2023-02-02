5d7644d087e7b9813e1e8fe2007b351d7baf5ba4
xjwalker <jesus.walker3000@gmail.com>
2023-02-02 13:31:33 -0800

    colorful names pinterest challenge complete
    correction on insertion sort algorithm

--- algo/insert-sort.py
+++ algo/insert-sort.py
@@ -1,16 +1,26 @@
-def insert_sort(some_array):
+def insertion_sort2(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+        print(*arr, sep=" ")
 
-    for j in range(len(some_array) - 1):
-        i = j + 1
-        while some_array[j] > some_array[i]:
-            aux = some_array[j]
-            some_array[j] = some_array[i]
-            some_array[i] = aux
-        i += 1
-    return some_array
+
+def third_is(arr):
+    for i in range(1, len(arr)):
+        while arr[i] < arr[i - 1] and i > 0:
+            arr[i], arr[i - 1] = arr[i - 1], arr[i]
+            i -= 1
+        print(*arr, sep=" ")
+    return arr
 
 
 if __name__ == '__main__':
-    arr = [1, 50, 3, 23, 2, 32]
-    print(arr)
-    print(insert_sort(arr))
+    arr = [1, 50, 3, 23, 2, 32, 30]
+    insertion_sort2(arr)
+    print('===')
+    arr = [1, 50, 3, 23, 2, 32, 30]
+    third_is(arr)
