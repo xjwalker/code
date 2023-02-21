27de18a22a7b22732c2d7762dc080110ab81e648
xjwalker <jesus.walker3000@gmail.com>
2023-02-21 13:23:02 -0800

    pinterest live code challenge
    from 12:51 to 13:21
    :C

---
+++ algo/pinterest_warmup/pinterest_interview.py
@@ -0,0 +1,34 @@
+k = [
+    [0, 1, 1, 0],  # 0
+    [0, 0, 1, 0],  # 1 a
+    [0, 0, 0, 0],  # 2
+    [0, 0, 1, 0]  # 3 a nobody.
+]
+
+
+def knows(i: int, j: int):
+    return k[i][j] == 1
+
+
+def get_celebrity(n: int):
+    celebs = set()
+    not_celeb = set()
+    for me in range(n):
+        for person in range(n):
+            if me != person:
+                if knows(me, person) and not knows(person, me) and person not in not_celeb:
+                    celebs.add(person)
+                    not_celeb.add(me)
+                elif knows(person, me):
+                    not_celeb.add(me)
+                    if person in celebs:
+                        celebs.remove(person)
+                elif not knows(me, person):
+                    not_celeb.add(person)
+                    if person in celebs:
+                        celebs.remove(person)
+
+    return -1 if len(celebs) == 0 else list(celebs)[0]
+
+
+print(get_celebrity(4))
